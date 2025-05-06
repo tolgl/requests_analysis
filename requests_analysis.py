a0900023514efd7d6563d331407eec802fe7019c
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-05-06 18:09:09 +0300

    add connect method yandex search

--- requests_analysis.py
+++ requests_analysis.py
@@ -1,4 +1,6 @@
 import pandas
+from ya_search import yandex_search
+from itertools import chain
 # import nltk
 # from nltk.tokenize import word_tokenize
 # from nltk.corpus import stopwords
@@ -19,7 +21,7 @@ import pandas
 #     for request in requests_list:
 #         file.write(f'{str(request)}\n')
 
-chars = [')', '(', ',', '–', '.', '*', '"', '%', '/', ';', ':', '{', '}', '@', '=', '#', '№', '!', '_', '¶', '°']
+chars = [')', '(', ',', '–', '*', '"', '%', '/', ';', ':', '{', '}', '@', '=', '#', '№', '!', '_', '¶', '°', '+']
 
 
 def requests_analysis():
@@ -27,19 +29,21 @@ def requests_analysis():
     requests_list = []
     list_list_words = []
     list_list_words_without_characters = []
-    list_brands = []
-    data = pandas.read_excel("2025-04-28 Запросы B2B.xlsx", engine='openpyxl')
+    list_products = []
+    list_products_2 = []
+    data = pandas.read_excel("2025-04-28 Запросы B2B — копия.xlsx", engine='openpyxl')
     df = pandas.DataFrame(data)
 
     for row in df.itertuples():
         requests_list.append(row[1])
+
     hhh = [
         'AURORA Интеллектуальное зарядное устройство SPRINT-6 (148 )вывыв %1fdsfs',
         'Уголок крепежный  оцинкованный 100х100х100х 2,5 мм – 293шт (ГОСТ 14918-2020)'
     ]
 
     for i in requests_list:
-        list_list_words.append(str(i).split())
+        list_list_words.append(str(i).lower().split())
 
     for list_words in list_list_words:
         for word in list_words:
@@ -50,10 +54,18 @@ def requests_analysis():
                 list_words.remove(bad_word)
         list_list_words_without_characters.append(list_words)
 
-    # print(list_list_words_without_characters[11591])
-    with open('result.txt', 'w', encoding='utf-8') as file:
-        for list_request_word in list_list_words_without_characters:
-            file.write(f'{str(list_request_word)}\n')
+    # print(list_list_words_without_characters)
+    list_requests_without_characters = [[' '.join(n)] for n in list_list_words_without_characters]
+    print(list_requests_without_characters)
+
+    for product in chain.from_iterable(list_requests_without_characters):
+        if len(yandex_search(query=product)) != 0:
+            list_products.append(yandex_search(query=product))
+        else:
+            list_products_2.append(product)
+
+    print(list_products)
+    print(list_products_2)
 
 
 requests_analysis()
