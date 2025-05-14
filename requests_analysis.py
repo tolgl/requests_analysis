bcf7d978acfeaa467323b5bb3255b146d21362b9
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-05-14 17:11:47 +0300

    editing code

--- requests_analysis.py
+++ requests_analysis.py
@@ -21,7 +21,7 @@ from itertools import chain
 #     for request in requests_list:
 #         file.write(f'{str(request)}\n')
 
-chars = [')', '(', ',', '–', '*', '"', '%', '/', ';', ':', '{', '}', '@', '=', '#', '№', '!', '_', '¶', '°', '+']
+chars = [')', '(', ',', '–', '*', '"', '%', ';', ':', '{', '}', '@', '=', '#', '№', '!', '_', '¶', '°', '+']
 
 
 def requests_analysis():
@@ -30,17 +30,11 @@ def requests_analysis():
     list_list_words = []
     list_list_words_without_characters = []
     list_products = []
-    list_products_2 = []
     data = pandas.read_excel("2025-04-28 Запросы B2B — копия.xlsx", engine='openpyxl')
     df = pandas.DataFrame(data)
 
     for row in df.itertuples():
         requests_list.append(row[1])
-
-    hhh = [
-        'AURORA Интеллектуальное зарядное устройство SPRINT-6 (148 )вывыв %1fdsfs',
-        'Уголок крепежный  оцинкованный 100х100х100х 2,5 мм – 293шт (ГОСТ 14918-2020)'
-    ]
 
     for i in requests_list:
         list_list_words.append(str(i).lower().split())
@@ -54,18 +48,19 @@ def requests_analysis():
                 list_words.remove(bad_word)
         list_list_words_without_characters.append(list_words)
 
-    # print(list_list_words_without_characters)
     list_requests_without_characters = [[' '.join(n)] for n in list_list_words_without_characters]
-    print(list_requests_without_characters)
+    list_requests_without_doubles = list(set(chain.from_iterable(list_requests_without_characters)))
+    print(list_requests_without_doubles)
 
-    for product in chain.from_iterable(list_requests_without_characters):
-        if len(yandex_search(query=product)) != 0:
+    for product in list_requests_without_doubles:
+        if len(yandex_search(query=product)) != 0 or yandex_search(query=product) != '':
             list_products.append(yandex_search(query=product))
         else:
-            list_products_2.append(product)
-
+            list_products.append(product)
     print(list_products)
-    print(list_products_2)
+    with open('result.txt', 'w', encoding='utf-8') as file:
+        for request in list_products:
+            file.write(f'{str(request)}\n')
 
 
 requests_analysis()
